8f3b6d1ecbd2cb7c8b8feebb5e87ed1fb57fcb53
hoshi yuki <au5.iphonex3@outlook.jp>
2022-06-15 03:27:48 +0000

    レイアウト完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
- has_one_attached :profile_image
+  has_one_attached :profile_image
   has_many :books, dependent: :destroy
   
   validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
